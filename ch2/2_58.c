a994f7b70ca6c8dd2aec1480eebbc56030a977ce
nameword <daisyat@163.com>
2019-10-21 21:29:08 +0800

    add file 2_58.c

---
+++ ch2/2_58.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef unsigned char* byte_pointer;
+
+int is_little_endian() {
+	int a = 1;
+	if ((byte_pointer)&a)
+		return 1;
+	return 0;
+}
+
+int main() {
+	printf("%d\n", is_little_endian());
+}
